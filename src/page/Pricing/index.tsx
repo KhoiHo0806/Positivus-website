6aa4a769b47b1a1cc85d1ad37be71f87ed9bafaf
s3877653 <hotranminhkhoi@gmail.com>
2024-12-22 22:18:09 +0700

    update

--- src/page/Pricing/index.tsx
+++ src/page/Pricing/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./pricing.module.css";
+import { useState } from "react";
 
 export const Pricing = () => {
   const pricingPlanList = [
@@ -41,6 +42,50 @@ export const Pricing = () => {
     },
   ];
 
+  const workingProcessList = [
+    {
+      name: "Are there any additional fees or charges that may apply?",
+      content:
+        "Our pricing plans include a wide range of digital marketing services, including website design and development, SEO, PPC advertising, social media marketing, content marketing, and more. We also offer custom packages that can be tailored to meet the specific needs of your business.",
+    },
+    {
+      name: "Can I change or cancel my plan at any time?",
+      content:
+        "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+    },
+    {
+      name: "Do you offer a free trial or consultation?",
+      content:
+        "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+    },
+    {
+      name: "How do you bill and invoice your clients?",
+      content:
+        "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+    },
+    {
+      name: "Are your services guaranteed to deliver results?",
+      content:
+        "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+    },
+    {
+      name: "Do you offer contract-based or monthly retainer-based pricing?",
+      content:
+        "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+    },
+  ];
+
+  const [expandIndexes, setExpandIndexes] = useState<number[]>([]);
+
+  const listExpandHandler = (index: number) => {
+    setExpandIndexes(
+      (prevIndexes) =>
+        prevIndexes.includes(index)
+          ? prevIndexes.filter((i) => i !== index) // Remove index if it's already in the list
+          : [...prevIndexes, index] // Add index if it's not in the list
+    );
+  };
+
   return (
     <div className={styles.pricingPageContainer}>
       <section className={styles.pricingIntroSection}>
@@ -79,6 +124,57 @@ export const Pricing = () => {
                       </div>
                     );
                   })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+
+      <section className={styles.questionSection}>
+        <h2 className="section-header">Frequently Asked Questions</h2>
+
+        <div className="home-woking-process-list">
+          {workingProcessList.map((item, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  height: expandIndexes.includes(index)
+                    ? "279px"
+                    : "calc((279px / 2) - 1px)",
+                  borderRadius: "45px",
+                  borderBottom: "5px solid black",
+                  overflow: "hidden",
+                  transition: "height 0.3s ease",
+                }}
+              >
+                <div
+                  className="working-process-item"
+                  style={{
+                    backgroundColor: expandIndexes.includes(index)
+                      ? "#b9ff66"
+                      : "#F3F3F3",
+                    transition: "background-color 0.5s ease",
+                  }}
+                >
+                  <div className="working-process-item-upper">
+                    <div className="header-and-index">
+                      <p className="item-header">{item.name}</p>
+                    </div>
+                    <img
+                      onClick={() => {
+                        listExpandHandler(index);
+                      }}
+                      src={`Home/workingProcess/${
+                        expandIndexes.includes(index)
+                          ? "shrinkIcon"
+                          : "expandIcon"
+                      }.png`}
+                      alt="working process icon"
+                    />
+                  </div>
+                  <p className="working-process-item-lower">{item.content}</p>
                 </div>
               </div>
             );
